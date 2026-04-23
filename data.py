b2d69f6ee278537fcd3828ac3935d8eea06923d3
lucas <lucas07_martins@hotmail.com>
2026-04-23 16:43:14 -0300

    subindo o projeto da sprint 7

--- data.py
+++ data.py
@@ -1,1 +1,7 @@
-# Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-7f9dc124-e86b-4149-b388-2b832120c6e9.containerhub.tripleten-services.com?lng=pt'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE =  '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
